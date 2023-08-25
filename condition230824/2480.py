dc0bf0c1aa3b44182237c85cd0c3250fc8863f28
y3rin <rladpfls0000@naver.com>
2023-08-25 20:12:50 +0900

    second plus commit

--- condition230824/2480.py
+++ condition230824/2480.py
@@ -1,13 +1,13 @@
 a, b, c = map(int, input().split())
 
-if a == b and b == c:
+if a == b and b == c: #a==b==c
     win = 10000+a*1000
 elif a == b or c == a:
     win = 1000 + a*100
 elif b == c:
     win = 1000+b*100
 else:
-    maximum = ((a) if a > c else (c)) if a > b else (b) if b > c else (c)
+    maximum = ((a) if a > c else (c)) if a > b else (b) if b > c else (c) #max(a,b,c)
     win = maximum*100
 
 print(win)
